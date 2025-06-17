f090717a8319520b26623ac38d391e50d3f0f974
Krishnakant Rai <krishnakant@atliq.com>
2025-06-17 18:12:34 +0530

    wip: worked on basic authentication and creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto, AuthSignInDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
-    return { msg: 'I am sign up' };
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          hash,
+        },
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+        },
+      });
+
+      return user;
+    } catch (error) {
+      throw new ForbiddenException(
+        'User failed to sign up.',
+      );
+    }
   }
 
-  signin() {
-    return { msg: 'I am sign in' };
+  async signin(dto: AuthSignInDto) {
+    console.log({ dto });
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+    if (!user) {
+      throw new ForbiddenException(
+        'User not found',
+      );
+    }
+
+    const pwMatch = await argon.verify(
+      user.hash,
+      dto.password,
+    );
+    if (!pwMatch) {
+      throw new ForbiddenException(
+        'Incorrect credentials',
+      );
+    }
+
+    return user;
   }
 }
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+
+export class AuthDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}
